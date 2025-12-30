1018f526101f8a00d2c5d2bf3db3e9ebe33ebcf2
melizadev <cesinelisa@gmail.com>
2025-12-30 02:31:50 -0500

    Refactor: Move BrowserRouter to main.jsx for better routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,17 @@ import "./App.css";
 import TaskForm from "./components/tasks/TaskForm.jsx";
 import TasksList from "./components/tasks/TasksList.jsx";
 import Header from "./components/Header.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <main className="bg-zinc-900 text-white">
       <div className="flex flex-col items-center justify-start min-h-screen h-full w-full p-4">
         <Header />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/task-flow/" element={<TasksList />} />
-            <Route path="/task-flow/create-task" element={<TaskForm />} />
-            <Route path="/task-flow/edit-task/:id" element={<TaskForm />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/task-flow/" element={<TasksList />} />
+          <Route path="/task-flow/create-task" element={<TaskForm />} />
+          <Route path="/task-flow/edit-task/:id" element={<TaskForm />} />
+        </Routes>
       </div>
     </main>
   );
