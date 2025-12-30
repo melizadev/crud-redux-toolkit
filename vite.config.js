fa065ba56b8d0200fe54ae09dc32f63456b67e26
melizadev <cesinelisa@gmail.com>
2025-12-30 02:14:25 -0500

    Fix: Update homepage URL in package.json and correct base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/task-flow",
+  base: "/task-flow/",
 });
